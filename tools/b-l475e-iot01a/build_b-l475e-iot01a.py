89bd2ef8cd72764fec8172522320b5ce7370e946
Biman Paul <biman.paul@samsung.com>
2021-04-04 23:34:34 +0530

    stm32l4: update build script

--- tools/b-l475e-iot01a/build_b-l475e-iot01a.py
+++ tools/b-l475e-iot01a/build_b-l475e-iot01a.py
@@ -15,6 +15,14 @@ BSP_PATH = os.path.join(os.environ["STDK_REF_PATH"],"bsp", BSP_NAME)
 IOT_APPS_PATH = os.path.join(os.environ["STDK_REF_PATH"], "apps", BSP_NAME, APP_NAME)
 CORE_PATH = os.path.join(os.environ["STDK_REF_PATH"],"iot-core")
 STDK_PATH = os.path.join(os.environ["STDK_REF_PATH"])
+STM32CUBEL4 = os.path.join(BSP_PATH, "stm32l4")
+
+print("BSP_PATH", BSP_PATH)
+print("IOT_APPS_PATH", IOT_APPS_PATH)
+print("CORE_PATH", CORE_PATH)
+print("STDK_PATH", STDK_PATH)
+print("STM32CUBEL4", STM32CUBEL4)
+
 
 if not (os.path.exists(BSP_PATH) and os.path.exists(IOT_APPS_PATH)):
     print("Fail to find path.")
@@ -42,9 +50,18 @@ os.system("sed -i.bak 's/unsigned/const unsigned/g' src/iot_root_ca.c")
 os.system("rm -f src/iot_root_ca.c.bak")
 os.chdir(os.path.join(STDK_PATH))
 
-os.chdir(os.path.join(BSP_PATH))
-build_cmd = os.path.join("make -DBSP_PATH=", BSP_PATH, " -DAPP_PATH=",IOT_APPS_PATH, "-DCORE_PATH", CORE_PATH)
+MAKEFILE_PATH = os.path.join(BSP_PATH, "apps", APP_NAME)
+print("MAKEFILE_PATH", MAKEFILE_PATH)
+os.chdir(MAKEFILE_PATH)
+print(os.getcwd())
+
+build_cmd = "make STM32CUBEL4=" + STM32CUBEL4 + " APP_PATH=" + MAKEFILE_PATH + " CORE_PATH=" +CORE_PATH
+# build_cmd = os.path.join("make")
+
+print("Build Command:", build_cmd)
 build_ret=os.system(build_cmd)
+
+print("Build Result:", build_ret)
 if build_ret:
     exit(1)
     
